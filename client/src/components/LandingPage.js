9d70445365dde76af693a80531d5fe847978fb2c
alphaone2468 <129504534+alphaone2468@users.noreply.github.com>
2025-02-10 23:37:42 +0530

    Updated Landing Page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 import firstImage from '../images/one.jpg'
 import "@fontsource/geist-sans";
 import cameraIcon from '../images/camera_icon.png'
@@ -8,36 +9,65 @@ import darkCameraIcon from '../images/dark_camera_icon.png'
 import darkRatingIcon from '../images/dark_rating_icon.png'
 import darkImprovementIcon from '../images/dark_improvement_icon.png'
 import darkReviewIcon from '../images/dark_review_icon.png'
+import { useNavigate } from 'react-router-dom';
 
 
 export default function LandingPage() {
+      const [buttonCss, setButtonCss] = useState({
+        boxShadow: "5px 5px black",
+      });
+      const navigate=useNavigate();
+      
+      const handleLogin = () => {
+        setButtonCss((prevStyle) => ({
+          ...prevStyle,
+          boxShadow: "0px 0px black",
+          transform: "translate(7px, 7px)"
+        }));
+    
+        setTimeout(()=>{
+          setButtonCss((prevStyle) => ({
+            ...prevStyle,
+            boxShadow: "5px 5px black",
+            transform: "translate(0px, 0px)"
+          }));
+        },150)
+      };
+
+      const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+
+      function callme(){
+        navbar.classList.toggle('active');
+        navbg.classList.toggle('active');
+        menuIcon.classList.toggle('bx-x');
+    }
   return (
       <div className='headingFont'>
           <div>
               <img src={firstImage} alt="" className='firstImage' loading='lazy' />
-
           </div>
           <div className="headingContent">
-              <h1 className='heading'>RateYourOutfit</h1>
-              <div style={{ height: "90vh" }} className='center'>
+                <div className="headingInfo">
+                    <h1 className='heading'>RateYourOutfit</h1>
+                </div>
+              <div style={{ height: "80vh" }} className='center'>
                   <div className='ta-c'>
                       <div className="tagLine">
-                          <h1>Elevate Your Style</h1>
-                          <h1>Today</h1>
+                          <h1>Elevate Your Style with RateYourOutfit</h1>
                       </div>
-                      <div className="tagLine tagLineContainer">
-                          <h1 className='tagLineText'>Unlock the power of personal style with expert tips and trends </h1>
-                          <h1 className='tagLineText'>tailored just for you, helping you shine with confidence . Discover the </h1>
-                          <h1 className='tagLineText'>perfect blend of comfort, elegance,and individuality to redefine your wardrobe</h1>
-                          <h1 className='tagLineText'> and express your true self effortlessly .</h1>
+                      <div className="tagLineContainer">
+                          <p className='tagLineText'>Upload your outfit, get rated by the community, and improve your fashion sense. Join the style revolution today!</p>                        
                       </div>
                       <div>
-                          <button className="startNowButton headingFont">Discover Now</button>
+                          <button className="startNowButton headingFont" onClick={()=>{navigate("/login")}} >Discover Now</button>
                       </div>
                   </div>
               </div>
           </div>
 
+          <div className="background">
 
           <div className='features'>
             <h1>Key Features</h1>
@@ -86,6 +116,20 @@ export default function LandingPage() {
                 <p className='featureContent'>Quickly upload your outfit photos with our user-friendly interface.</p>
             </div>
             </div>
+          </div>
+
+
+
+          <div className="contact">
+            <h1 className='ta-c'>Get in Touch</h1>
+            <p className='ta-c'>Have a question or want to learn more about our platform? We'd love to</p>
+            <div className="contactInfo">
+                <input type="email" name="" id="" placeholder='alpha@gmail.com' />
+                <textarea type="message" name="" id="" placeholder='message' />
+                <button className='contactUsButton' style={buttonCss} onClick={handleLogin}>submit</button>
+            </div>
+          </div>
+
           </div>
       </div>
   )
--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -8,6 +8,9 @@ export default function About() {
   const [string,setstring]=useState("")
   const [img,setimg]=useState("")
   const [loading,setloading]=useState(false);
+    const [buttonCss, setButtonCss] = useState({
+      boxShadow: "5px 5px black",
+    });
   //console.log(img);
   async function callme(e) {
     const file=e.target.files[0];
@@ -18,35 +21,23 @@ export default function About() {
     document.getElementById("hideme").style.display="none";
     setstring(base64)
   }
-  async function handlesubmit(e) {
-    setloading(true);
-    const obj={
-        owner:localStorage.getItem("ryo"),
-        image:string,
-        ratings:1,
-        comments:[{ratings:[]},{count:0}]
+  async function handleSubmit() {
+    setButtonCss((prevStyle) => ({
+      ...prevStyle,
+      boxShadow: "0px 0px black",
+      transform: "translate(7px, 7px)"
+    }));
 
-    }
-    const f=await fetch("http://localhost:5000/postimage",{
-        method:"POST",
-        body:JSON.stringify(obj),
-        headers:{
-            "Content-Type":"application/json"
-        }
-    })
-    const res=await f.json()
-    //console.log(res)
-    if(res.success=="up"){
-      //console.log("calling ")
-      document.getElementById("successUpload").style.display="block";
-      document.getElementById("hideme").style.display="block";
-      setloading(false);
-    }
-    
-    //console.log(obj)
+    setTimeout(()=>{
+      setButtonCss((prevStyle) => ({
+        ...prevStyle,
+        boxShadow: "5px 5px black",
+        transform: "translate(0px, 0px)"
+      }));
+    },150)
   }
   return (
-    <div>
+    <div className='navbarContainer'>
       <Navbar/>
       <div className='upload'>
       <h1 className='ta-c headingFont'>Upload Your Image</h1>
@@ -59,8 +50,8 @@ export default function About() {
       <img src={img} alt="" className='postimage someMarginInLeftAndRight none' id="hidemeinstart"/>
       <p id="successUpload" className='none makecenter2 makebold1'><strong>Image Posted Successfully</strong></p>
       {(loading)? <p className='aligncenter'><strong>posting.....</strong></p>:""}
-      <div className="butcon">
-      <button onClick={handlesubmit} className='makebold'>Submit</button>
+      <div className="uploadContainer">
+      <button onClick={handleSubmit} className='upload_button' style={buttonCss}>Submit</button>
       </div>
       </div>
     </div>
